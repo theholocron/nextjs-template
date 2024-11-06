e7916c862d28feb70b37acffe3be9026772dcc93
Newton <5769156+iamnewton@users.noreply.github.com>
2024-11-06 15:27:12 -0800

    chore: fix cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,7 +4,14 @@ import { defineConfig } from "cypress";
  * @see https://docs.cypress.io/app/references/configuration
  */
 export default defineConfig({
+	component: {
+		devServer: {
+			framework: "next",
+			bundler: "webpack",
+		},
+	},
 	e2e: {
+		setupNodeEvents(on, config) {},
 		baseUrl: "http://localhost:3000/", // this is the default port for `next` that runs a server
 		specPattern: "src/**/*.{cy.js,cy.ts}",
 		supportFile: false,
