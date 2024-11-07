ecd2a0a4982bac31afa31bdd55c11c14206a9e22
iamnewton <iamnewton@users.noreply.github.com>
2024-11-07 23:35:42 +0000

    chore: fix linting issues
    Signed-off-by: super-linter <super-linter@super-linter.dev>

--- app/msw/handlers.ts
+++ app/msw/handlers.ts
@@ -1,7 +1,4 @@
 import { handlers as authHandlers } from "../../src/auth/handlers";
 import { handlers as taskHandlers } from "../../src/tasks/handlers";
 
-export const handlers = [
-	...taskHandlers,
-	...authHandlers,
-];
+export const handlers = [...taskHandlers, ...authHandlers];
