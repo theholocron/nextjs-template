0a6a02255402e36412e415821c787bfbd5fe1a5d
Newton <5769156+iamnewton@users.noreply.github.com>
2024-11-07 15:14:58 -0800

    chore: fix build
    Signed-off-by: Newton <5769156+iamnewton@users.noreply.github.com>

--- lighthouse.config.js
+++ lighthouse.config.js
@@ -1,7 +1,13 @@
-module.exports = {
+/**
+ * @see https://github.com/GoogleChrome/lighthouse-ci/blob/main/docs/configuration.md
+ * @type {LHCI.LighthouseCiConfig}
+ */
+const config = {
 	ci: {
 		upload: {
 			target: "temporary-public-storage",
 		},
 	},
 };
+
+export default config;
--- src/login/login.tsx
+++ src/login/login.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import * as React from "react";
 import type { Credentials } from "../auth";
 import "./login.css";
 
@@ -8,11 +7,8 @@ export interface LoginFormProps {
 	onSubmit: (formData: Credentials) => void;
 }
 
-export function LoginForm (props: LoginFormProps) {
-	const {
-		onSubmit,
-		...rest
-	} = props;
+export function LoginForm(props: LoginFormProps) {
+	const { onSubmit, ...rest } = props;
 
 	return (
 		<form
@@ -76,7 +72,7 @@ export interface LoginProps {
 	onLogIn: (credentials: Credentials) => void;
 }
 
-export function Login (props: LoginProps) {
+export function Login(props: LoginProps) {
 	return (
 		<div className="page lists-show">
 			<div className="loginscreen">
