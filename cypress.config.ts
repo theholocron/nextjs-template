99da481438ff20b6365c3b8fee7a848505079176
Newton <5769156+iamnewton@users.noreply.github.com>
2025-07-31 14:23:52 -0700

    chore: fix cypress
    Signed-off-by: Newton <5769156+iamnewton@users.noreply.github.com>

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "cypress";
+// import { mergeConfig, loadEnv } from "vite";
 
 /*
  * @see https://docs.cypress.io/app/references/configuration
@@ -8,14 +9,33 @@ export default defineConfig({
 		devServer: {
 			framework: "next",
 			bundler: "vite",
+			/*
+			viteConfig: () => {
+				const viteConfig = require("./vite.config.ts");
+				const conf = {
+					define: {
+						"process.env": loadEnv("development", process.cwd(), "VITE"),
+					},
+					server: {
+						/**
+						* start the CT dev server on a different port than the full RWA
+						* so users can switch between CT and E2E testing without having to
+						* stop/start the RWA dev server.
+						port: 3002,
+					},
+				};
+				const resolvedViteConfig = mergeConfig(viteConfig, conf);
+				return resolvedViteConfig;
+			},
+			*/
 		},
 	},
 	e2e: {
 		// setupNodeEvents(on, config) {},
-		baseUrl: "http://localhost:3000/", // this is the default port for `next` that runs a server
+		// baseUrl: "http://localhost:3000/", // this is the default port for `next` that runs a server
 		specPattern: "src/**/*.{cy.js,cy.ts}",
 		supportFile: false,
-		retries: 2,
+		// retries: 2,
 	},
 	projectId: "etpgyt",
 });
