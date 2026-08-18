2ac70e78dd00b73954ac9b8b15ec99993a731c84
Newton <5769156+iamnewton@users.noreply.github.com>
2026-08-18 10:06:29 -0700

    chore: 🔧 require interaction, a11y, and user flow checks
    Signed-off-by: Newton <5769156+iamnewton@users.noreply.github.com>

--- holocron.config.ts
+++ holocron.config.ts
@@ -13,6 +13,11 @@ export default defineConfig({
 		topics: ["nextjs", "react", "template", "typescript"],
 		...repo,
 		protection: "strict",
+		requiredChecks: [
+			"Test / Test Interactions and Accessibility",
+			"Test / Test User Flow",
+			"Test / Run Storybook interaction tests",
+		],
 		properties: {
 			...repo.properties,
 			runtime_environment: "browser",
